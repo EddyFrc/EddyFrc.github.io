c9036b7edff4ec09685429ddbd74b64927e2c789
EddyFrc <eddyfrancou@gmail.com>
2025-05-01 16:56:48 +0200

    Dropdown fonctionnel pour le thème

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,28 +1,5 @@
-const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-
-/**
- * Applique le thème le plus approprié dans cette situation<br>
- * Cette fonction doit être appelée quand le thème du système est modifié,
- * y compris à chaque chargement de la page.
- * @param themeOrEvent Evénement de catégorie `change` affecté à un `MediaQueryList`, ou
- * carrément un `MediaQueryList`
- */
-function onSystemThemeChange(themeOrEvent) {
-    // Par défaut, prend la valeur 'system' pour basculer sur le thème du système
-    const theme = localStorage.getItem("theme") ?? 'system';
-
-    // Si le thème n'est pas 'system', basculer sur le thème spécifique choisi
-    if (theme !== 'system') {
-        applyTheme(theme);
-        return;
-    }
-
-    // Si on est ici c'est que le thème choisi n'est pas 'system'.
-    if (themeOrEvent.matches)
-        applyTheme('dark');
-    else
-        applyTheme('light');
-}
+const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+const themeDropdown = document.getElementById('theme-selector');
 
 /**
  * Applique un thème sombre ou clair (`'dark'` ou `'light'`)
@@ -40,6 +17,44 @@ function applyTheme(theme) {
     }
 }
 
+/**
+ * Applique le thème le plus approprié dans cette situation<br>
+ * Cette fonction doit être appelée quand le thème du système est modifié,
+ * y compris à chaque chargement de la page.
+ * @param themeOrEvent Evénement de catégorie `change` affecté à un `MediaQueryList`, ou
+ * carrément un `MediaQueryList`
+ */
+function onSystemThemeChange(themeOrEvent) {
+    // Par défaut, prend la valeur 'system' pour basculer sur le thème du système
+    const theme = localStorage.getItem('theme') ?? 'system';
+
+    // Si le thème n'est pas 'system', basculer sur le thème spécifique choisi
+    if (theme !== 'system') {
+        // 0 = 'light'
+        // 1 = 'dark'
+        themeDropdown.options[theme === 'light' ? 0 : 1].selected = true;
+        applyTheme(theme);
+        return;
+    }
+
+    // Si on est ici c'est que le thème choisi est 'system'.
+    // 2 = 'system'
+    themeDropdown.options[2].selected = true;
+    if (themeOrEvent.matches)
+        applyTheme('dark');
+    else
+        applyTheme('light');
+}
+
+/**
+ * Cette fonction doit être appelée quand l'état du sélecteur de thème change.
+ */
+function onThemeDropdownChange() {
+    localStorage.setItem('theme', themeDropdown.options[themeDropdown.selectedIndex].value);
+    onSystemThemeChange(darkThemeMq);
+}
+
 darkThemeMq.addEventListener('change', onSystemThemeChange);
+themeDropdown.addEventListener('change', onThemeDropdownChange);
 
 onSystemThemeChange(darkThemeMq);
--- view/block/navbar.view.php
+++ view/block/navbar.view.php
@@ -5,4 +5,12 @@
         <a href="/?page=realisations" id="nav-realisations">Réalisations</a>
         <a href="/?page=contact" id="nav-contact">Contact</a>
     </nav>
+    <div>
+        <label for="theme-selector">Thème :</label>
+        <select id="theme-selector">
+            <option value="light">Clair</option>
+            <option value="dark">Sombre</option>
+            <option value="system">Système</option>
+        </select>
+    </div>
 </header>
